230732145595d186362d4a635923ba2c2d7f70d3
aizhan-ayapbergenova <aizhan@robarin.pro>
2021-10-03 14:58:30 +0600

    question controller spec show

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -4,9 +4,7 @@ RSpec.describe QuestionsController, type: :controller do
   describe 'GET #index' do
     let(:questions) { create_list(:question, 2) }
 
-    before do
-      get :index
-    end
+    before { get :index }
 
     it 'fills in array of all questions' do
       expect(assigns(:questions)).to match_array(questions)
@@ -14,6 +12,18 @@ RSpec.describe QuestionsController, type: :controller do
 
     it 'renders index view' do
       expect(response).to render_template :index
+    end
+  end
+
+  describe 'GET #show' do
+    let(:question) { create(:question) }
+    before { get :show, params: { id: question} }
+    it 'assigns the requested question to @question' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'renders show view' do
+      expect(response).to render_template :show
     end
   end
 end
